4fa96b2df576462f33e65ae792ea7d62d7d38439
MJKeo <mkeohane@gatech.edu>
2025-12-04 23:40:23 -0500

    [App] Spiced up mobile not supported page

--- interview-bot-app/screens/mobile-not-supported-screen/mobile-not-supported-screen.tsx
+++ interview-bot-app/screens/mobile-not-supported-screen/mobile-not-supported-screen.tsx
@@ -2,6 +2,9 @@
 
 import "./mobile-not-supported-screen.css";
 import sadPhoneImage from "../../images/sad_phone.png";
+import interviewIcon from "../../images/interview-icon-white.png";
+import githubIcon from "../../images/github-mark.png";
+import linkedinIcon from "../../images/InBug-Black.png";
 
 /**
  * Screen component that displays a message indicating that mobile devices are not supported.
@@ -12,6 +15,11 @@ export default function MobileNotSupportedScreen() {
     <div className="mobile-not-supported-container">
       {/* Interview Pro top bar */}
       <div className="interview-pro-badge">
+        <img 
+          src={interviewIcon.src} 
+          alt="Interview Pro icon" 
+          className="interview-pro-icon"
+        />
         Interview Pro
       </div>
 
@@ -25,8 +33,40 @@ export default function MobileNotSupportedScreen() {
 
         {/* Header text */}
         <h1 className="mobile-not-supported-header">
-          Mobile devices are not supported
+          Mobile devices are not supported, but check out these cool resources!
         </h1>
+
+        {/* Social icons */}
+        <div className="mobile-social-icons">
+          <button
+            type="button"
+            className="mobile-social-icon"
+            aria-label="GitHub"
+            onClick={() => {
+              window.open("https://github.com/MJKeo", "_blank", "noopener,noreferrer");
+            }}
+          >
+            <img
+              src={githubIcon.src}
+              alt="GitHub"
+              className="mobile-social-icon-img"
+            />
+          </button>
+          <button
+            type="button"
+            className="mobile-social-icon"
+            aria-label="LinkedIn"
+            onClick={() => {
+              window.open("https://www.linkedin.com/in/michael-keohane", "_blank", "noopener,noreferrer");
+            }}
+          >
+            <img
+              src={linkedinIcon.src}
+              alt="LinkedIn"
+              className="mobile-social-icon-img"
+            />
+          </button>
+        </div>
       </div>
     </div>
   );
